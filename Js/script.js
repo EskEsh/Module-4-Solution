6c8bcfa27e383a348d2b0044f7eeaa68a9473f93
EskEsh <e.eskender@gmail.com>
2024-10-12 18:02:52 +0300

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -1,20 +1,17 @@
-(function (window) {
+// script.js 
+
+(function () {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-helloSpeaker.speak(); 
-byeSpeaker.speak();
-
-function (name) {
 for (var i = 0; i < names.length; i++) {
-var firstLetter = function (name) {
-return charAt(0).toLowerCase; 
-}; 
+var firstLetter = names[i].charAt(0).toLowerCase(); 
+
 if (firstLetter == 'j') {
- console.log(byeSpeaker.speak(names[i]));
-   } else {
- console.log(helloSpeaker.speak(names[i])); 
+ byeSpeaker(names[i]);
+   } 
+else {
+ helloSpeaker(names[i]); 
    }
  }
-}
 
-})(window);
+})();
